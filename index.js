ad5a7c98f92498fa4c8a3d10121698cd3c9fce4a
Harpreet Singh <hurpreet@icloud.com>
2023-04-08 23:47:36 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const drivers= ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(array, string)
+{
+   return array.filter(array => array.toUpperCase()=== string.toUpperCase())
+}
+
+
+function fuzzyMatch(array, string)
+{
+    return array.filter(function (name){
+        if(name[0]===string[0])
+        {
+            return name;
+        }
+    })
+}
+function matchName(array, string)
+{
+   
+    return array.filter(array=> array.name === string)
+     
+}
